634a8d7d1ef0fb4c9d5e03c0da7ba238e8f41282
FernandesKA <fernandes.kir@yandex.ru>
2026-01-20 23:37:37 +0300

    [+] add dts for sipeed m1s devboard

---
+++ buildroot_external/board/sipeed/m1s/dts/bouffalolab/.bl808-pine64-ox64.dtb.cmd
@@ -0,0 +1,15 @@
+savedcmd_arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dtb := /home/builder/buildroot/buildroot/output/host/bin/ccache /usr/bin/gcc -O2 -isystem /home/builder/buildroot/buildroot/output/host/include -L/home/builder/buildroot/buildroot/output/host/lib -Wl,-rpath,/home/builder/buildroot/buildroot/output/host/lib -E -Wp,-MMD,arch/riscv/boot/dts/bouffalolab/.bl808-pine64-ox64.dtb.d.pre.tmp -nostdinc -I./scripts/dtc/include-prefixes -undef -D__DTS__ -x assembler-with-cpp -o arch/riscv/boot/dts/bouffalolab/.bl808-pine64-ox64.dtb.dts.tmp arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dts ; ./scripts/dtc/dtc -o arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dtb -b 0 -iarch/riscv/boot/dts/bouffalolab/ -i./scripts/dtc/include-prefixes -@ -Wno-interrupt_provider -Wno-unique_unit_address -Wno-unit_address_vs_reg -Wno-avoid_unnecessary_addr_size -Wno-alias_paths -Wno-graph_child_address -Wno-simple_bus_reg   -Wno-interrupt_provider -Wno-unique_unit_address -Wno-unit_address_vs_reg -Wno-avoid_unnecessary_addr_size -Wno-alias_paths -Wno-graph_child_address -Wno-simple_bus_reg   -d arch/riscv/boot/dts/bouffalolab/.bl808-pine64-ox64.dtb.d.dtc.tmp arch/riscv/boot/dts/bouffalolab/.bl808-pine64-ox64.dtb.dts.tmp ; cat arch/riscv/boot/dts/bouffalolab/.bl808-pine64-ox64.dtb.d.pre.tmp arch/riscv/boot/dts/bouffalolab/.bl808-pine64-ox64.dtb.d.dtc.tmp > arch/riscv/boot/dts/bouffalolab/.bl808-pine64-ox64.dtb.d
+
+source_arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dtb := arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dts
+
+deps_arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dtb := \
+  arch/riscv/boot/dts/bouffalolab/bl808.dtsi \
+  scripts/dtc/include-prefixes/dt-bindings/interrupt-controller/irq.h \
+  scripts/dtc/include-prefixes/dt-bindings/mailbox/bflb-ipc.h \
+  scripts/dtc/include-prefixes/dt-bindings/pinctrl/pinctrl-bflb.h \
+  scripts/dtc/include-prefixes/dt-bindings/gpio/gpio.h \
+  scripts/dtc/include-prefixes/dt-bindings/leds/common.h \
+
+arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dtb: $(deps_arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dtb)
+
+$(deps_arch/riscv/boot/dts/bouffalolab/bl808-pine64-ox64.dtb):
---
+++ buildroot_external/board/sipeed/m1s/flashing-instruction.md
@@ -0,0 +1,28 @@
+
+## Flashing Instructions
+
+[](https://github.com/openbouffalo/buildroot_bouffalo#flashing-instructions)
+
+Download your prefered image above and extract the files.
+
+- Get DevCube **1.8.3** from [https://openbouffalo.org/static-assets/bldevcube/BouffaloLabDevCube-v1.8.3.zip](https://openbouffalo.org/static-assets/bldevcube/BouffaloLabDevCube-v1.8.3.zip) (normal download location is [http://dev.bouffalolab.com/download](http://dev.bouffalolab.com/download) but 1.8.4 and later do not work, see #60)
+- Connect BL808 board via serial port to your PC
+- Set BL808 board to programming mode
+    - Press BOOT button when reseting or applying power
+    - Release BOOT button
+- Run DevCube, select [BL808], and switch to [MCU] page
+- Select the uart port and set baudrate with 2000000
+    - UART TX is physical pin 1/GPIO 14.
+    - UART RX is physical pin 2/GPIO 15.
+- M0 Group[Group0] Image Addr [0x58000000] [PATH to m0_lowload_bl808_m0.bin]
+- D0 Group[Group0] Image Addr [0x58100000] [PATH to d0_lowload_bl808_d0.bin]
+- Click 'Create & Download' and wait until it's done
+- Switch to [IOT] page
+- Enable 'Single Download', set Address with 0x800000, choose [bl808-firmware.bin]
+- Click 'Create & Download' again and wait until it's done
+- flash the sdcard-pine64-*.img.xz to your SD card (you can use dd (after uncompressing) or [https://github.com/balena-io/etcher](https://github.com/balena-io/etcher))
+- Serial Console access:
+    - UART TX is physical pin 32/GPIO 16.
+    - UART RX is physical pin 31/GPIO 17.
+    - Baud 2000000.
+- Enjoy!
